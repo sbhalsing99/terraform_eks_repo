b06e2db49d64a784cde8d4b949a9ee432e30b347
sbhalsing99 <bhalsing0899@gmail.com>
2025-12-02 19:59:14 +0530

    commit 5

--- eks/eks.tf
+++ eks/eks.tf
@@ -6,32 +6,23 @@ module "eks" {
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
-  
-
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # EKS Managed Node Group(s)
+  control_plane_subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
-
-    instance_types = ["t2.medium"]
-
+    instance_types = ["t3.medium"]
     attach_cluster_primary_security_group = true
-
   }
 
-
   eks_managed_node_groups = {
-
     bankapp-ng = {
       min_size     = 2
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t2.medium"]
-      capacity_type  = "SPOT"
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
 
       tags = {
         ExtraTag = "MyCluster"
@@ -40,6 +31,4 @@ module "eks" {
   }
 
   tags = local.tags
-
-
 }
