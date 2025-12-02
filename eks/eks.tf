4f95fd2e70b662c0b6c7ffa432dae9458b22236c
sbhalsing99 <bhalsing0899@gmail.com>
2025-12-02 19:19:38 +0530

    commit 2

--- eks/eks.tf
+++ eks/eks.tf
@@ -6,17 +6,7 @@ module "eks" {
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
+  
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
